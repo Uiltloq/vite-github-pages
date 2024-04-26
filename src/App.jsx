42309783980cc95c95dd4841d3bd3380c0d72792
ll <uiltloq@gmail.com>
2024-04-26 23:55:41 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
 import './App.css'
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+
+  useEffect(()=>{
+    tg.ready();
+  },[]);
+
+  const handleClose = () => {
+    tg.close();
+  }
+
   return (
     <>
-      <div>WORK</div>
+      <div>
+        В разработке...
+        <button onClick={handleClose}>Закрыть окно</button>
+      </div>
     </>
   )
 }
